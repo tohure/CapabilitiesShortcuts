e5e846bbbcfdb49b951e08f35138b4f8501b908b
Carlo Huamán <cr.htorres@gmail.com>
2021-11-13 00:15:42 -0500

    set shortcuts static, dynamic, pinned

---
+++ app/src/main/java/io/tohure/capabilitiesdemo/util/ShortcutsUtil.kt
@@ -0,0 +1,53 @@
+package io.tohure.capabilitiesdemo.util
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import io.tohure.capabilitiesdemo.R
+
+object ShortcutsUtil {
+
+    fun setDynamicShortcut(context: Context) {
+        ShortcutManagerCompat.pushDynamicShortcut(context, getShortCutInfo(context))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun setPinnedShortcut(context: Context) {
+        if (ShortcutManagerCompat.isRequestPinShortcutSupported(context)) {
+            val pinnedShortcutIntent =
+                ShortcutManagerCompat.createShortcutResultIntent(context, getShortCutInfo(context))
+
+            val pinnedShortcutCallback = PendingIntent.getBroadcast(
+                context,
+                0,
+                pinnedShortcutIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            ShortcutManagerCompat.requestPinShortcut(
+                context,
+                getShortCutInfo(context),
+                pinnedShortcutCallback.intentSender
+            )
+        }
+    }
+
+    private fun getShortCutInfo(context: Context): ShortcutInfoCompat {
+        return ShortcutInfoCompat.Builder(context, "website")
+                .setShortLabel("Abrir Github")
+                .setLongLabel("Abrir Github de ejemplo")
+                .setIcon(IconCompat.createWithResource(context, R.drawable.ic_webview))
+                .setIntent(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/Tohure/CapabilitiesShortcuts")
+                    )
+                )
+                .build()
+    }
+}
--- app/src/main/java/io/tohure/capabilitiesdemo/view/account/AccountFragment.kt
+++ app/src/main/java/io/tohure/capabilitiesdemo/view/account/AccountFragment.kt
@@ -1,16 +1,20 @@
 package io.tohure.capabilitiesdemo.view.account
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.tohure.capabilitiesdemo.databinding.FragmentAccountBinding
+import io.tohure.capabilitiesdemo.util.ShortcutsUtil
 
 class AccountFragment : Fragment() {
 
     private var _binding: FragmentAccountBinding? = null
     private val binding get() = _binding!!
+    var toastMsg = "Tu sistema no soporta Shortcuts"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,26 @@ class AccountFragment : Fragment() {
         if (_binding == null)
             _binding = FragmentAccountBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnWebsite.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 25) {
+                ShortcutsUtil.setDynamicShortcut(requireContext())
+                toastMsg = "Dynamic Shortcut creado!"
+            }
+            Toast.makeText(context, toastMsg, Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnWebsiteLink.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 28) {
+                ShortcutsUtil.setPinnedShortcut(requireContext())
+                toastMsg = "Pinned Shortcut creado!"
+            }
+            Toast.makeText(context, toastMsg, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
